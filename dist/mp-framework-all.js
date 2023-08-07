d6acc93f4d5b0dacc04493441a45893949a14988
mowglymx <mowglymx@gmail.com>
2023-08-06 19:00:35 -0600

    Updates

--- dist/mp-framework-all.js
+++ dist/mp-framework-all.js
@@ -143,5 +143,32 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 });
+
+/*************************************************************************** 
+  MP Buttons
+****************************************************************************/
+
+document.addEventListener('DOMContentLoaded', function () {
+  const mpButtons = document.querySelectorAll('.mp-button');
+  mpButtons.forEach(button => {
+    const children = Array.from(button.childNodes);
+    let firstIIndex = children.findIndex(node => node.nodeType === Node.ELEMENT_NODE && node.tagName.toLowerCase() === 'i');
+    let firstTextIndex = children.findIndex(node => node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== '');
+    if (firstIIndex !== -1 && firstTextIndex !== -1) {
+      if (firstIIndex < firstTextIndex) {
+        button.classList.add('mp-icon-start');
+      } else {
+        button.classList.add('mp-icon-end');
+      }
+    } else if (firstIIndex !== -1) {
+      // Only <i> element is present, can be at start or end
+      if (firstIIndex === 0) {
+        button.classList.add('mp-icon-start');
+      } else {
+        button.classList.add('mp-icon-end');
+      }
+    }
+  });
+});
 /******/ })()
 ;
